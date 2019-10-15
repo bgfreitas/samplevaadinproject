3428b4abf0f21b165f31f451c84c9aff68c8591e
Bruno Freitas <bruno.freitas@unilabs.com>
2019-10-15 08:56:53 +0100

    Use of FullCalendar Addon (For testing)

--- src/main/java/com/unilabs/brunofreitas/spring/MainView.java
+++ src/main/java/com/unilabs/brunofreitas/spring/MainView.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
+import org.vaadin.stefan.fullcalendar.FullCalendar;
+import org.vaadin.stefan.fullcalendar.FullCalendarBuilder;
 
 @Route
 @PWA(name = "Project Base for Vaadin Flow with Spring", shortName = "Project Base")
@@ -16,6 +18,10 @@ public class MainView extends VerticalLayout {
         Button button = new Button("Click me",
                 e -> Notification.show(bean.getMessage()));
         add(button);
+
+        FullCalendar calendar = FullCalendarBuilder.create().build();
+        calendar.setSizeFull();
+        add(calendar);
     }
 
 }
